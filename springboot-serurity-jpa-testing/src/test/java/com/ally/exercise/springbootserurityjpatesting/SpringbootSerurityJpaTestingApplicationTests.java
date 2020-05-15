fdc9cd8c238b4f8672947a95d6dbbacf064a840f
seidabdu <seidpeace@gmail.com>
2020-05-15 14:39:03 -0400

    Jacocco and Junit test cases are added.

--- springboot-serurity-jpa-testing/src/test/java/com/ally/exercise/springbootserurityjpatesting/SpringbootSerurityJpaTestingApplicationTests.java
+++ springboot-serurity-jpa-testing/src/test/java/com/ally/exercise/springbootserurityjpatesting/SpringbootSerurityJpaTestingApplicationTests.java
@@ -1,14 +1,14 @@
 package com.ally.exercise.springbootserurityjpatesting;
 
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringbootSerurityJpaTestingApplicationTests {
+public class SpringbootSerurityJpaTestingApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
---
+++ springboot-serurity-jpa-testing/src/test/java/com/ally/exercise/springbootserurityjpatesting/service/CustomerServiceTests.java
@@ -0,0 +1,45 @@
+package com.ally.exercise.springbootserurityjpatesting.service;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.ally.exercise.springbootserurityjpatesting.model.Customer;
+import com.ally.exercise.springbootserurityjpatesting.repository.CustomerRepository;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+public class CustomerServiceTests {
+
+	@MockBean
+	private CustomerRepository customerRepository;
+
+	@Autowired
+	private CustomerService customerService;
+
+	@Test
+	public void testGetCustomers() {
+		List<Customer> customerList = new ArrayList<Customer>();
+		Customer cust1 = new Customer();
+		cust1.setCity("Mekelle");
+		Customer cust2 = new Customer();
+		cust2.setCity("Adigrat");
+		customerList.add(cust1);
+		customerList.add(cust2);
+
+		when(customerRepository.findAll()).thenReturn(customerList);
+
+		List<Customer> result = customerService.getAllCustomers();
+		assertEquals(2, result.size());
+	}
+
+}
